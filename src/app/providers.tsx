bc02c97b06f6b6211047a6e30a6f94230ea7d8c6
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-02-09 11:44:53 -0800

    auth in server actions and sign in, sign out, and checking auth status

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,18 @@
-"use client"
+"use client";
 
-import { NextUIProvider } from "@nextui-org/react"
+import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
 
 // This file allows different states to be shared across the app using the Context API.
 
 interface ProvidersProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
-export default function Providers({ children}: ProvidersProps) {
-    return (
-        <NextUIProvider>
-            {children}
-        </NextUIProvider>
-    )
+export default function Providers({ children }: ProvidersProps) {
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  );
 }
