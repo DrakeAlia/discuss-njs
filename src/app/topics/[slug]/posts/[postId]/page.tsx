ee8dfd52286a80ac092c4c433e11f9b72649f6bd
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-02-15 18:32:11 -0800

    adding a loading skeleton

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Suspense } from "react";
 import paths from "@/paths";
 import PostShow from "@/components/posts/post-show";
+import PostShowLoading from "@/components/posts/post-show-loading";
 import CommentCreateForm from "@/components/comments/comment-create-form";
 import CommentList from "@/components/comments/comment-list";
 import { fetchCommentsByPostId } from "@/db/queries/comments";
@@ -21,7 +22,7 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
         {/* {`< `}Back to {slug} */}
         {`< Back to ${slug}`}
       </Link>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<PostShowLoading />}>
         <PostShow postId={postId} />
       </Suspense>
       <CommentCreateForm postId={postId} startOpen />
--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -8,7 +8,7 @@ export type CommentWithAuthor = Comment & {
 
 export const fetchCommentsByPostId = cache(
   (postId: string): Promise<CommentWithAuthor[]> => {
-    console.log("Making a query!");
+    // console.log("Making a query!");
     return db.comment.findMany({
       where: {
         postId,
